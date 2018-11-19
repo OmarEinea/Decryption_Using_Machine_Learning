a724f4b9bbdb156388e45a7675e5425e5d22021b
Omar Einea <eineao@gmail.com>
2018-11-19 11:54:53 +0400

    Added shifting encryption & Bidirectional layer

--- encrypt.py
+++ encrypt.py
@@ -11,3 +11,7 @@ def vigenere_cipher(string):
         chr(__chars.range_start + (ord(char) + shift + 1 - __chars.range_start) % __chars.range_length)
         for shift, char in enumerate(string)
     ])
+
+def shifting_cipher(string, by=1):
+    at = by % len(string)
+    return string[at:] + string[:at]
