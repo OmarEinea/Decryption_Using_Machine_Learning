b19fed560ac0caf58384033338c73071cd0e627e
Omar Einea <eineao@gmail.com>
2018-11-27 20:24:20 +0400

    Simplify characters object & hide useless imports

--- tools.py
+++ tools.py
@@ -1,36 +1,28 @@
-from keras.preprocessing.text import Tokenizer
-import string, random, numpy
+from keras.preprocessing.text import Tokenizer as __Tokenizer
+import random as __random, numpy as __numpy, string as __string
 
 
-class __Characters:
-    def __init__(self, chars=string.ascii_letters):
-        self.set_chars(chars)
-
-    def set_chars(self, chars):
-        self.length = len(chars)
-        self.list = chars
-
-
-__tokenizer = Tokenizer(char_level=True, lower=False)
-__tokenizer.fit_on_texts(string.printable[:-2])
-__chars = __Characters()
+__tokenizer = __Tokenizer(char_level=True, lower=False)
+__tokenizer.fit_on_texts(__string.printable[:-2])
+__chars = list(__string.ascii_letters)
 
 
 def set_characters(chars):
-    __chars.set_chars(chars)
+    __chars.clear()
+    __chars.extend(chars)
 
 
 def generate_text(length, limit=10000):
     text = set()
     for _ in range(int(limit*1.2)):
-        text.add(''.join(random.sample(__chars.list, length)))
+        text.add(''.join(__random.sample(__chars, length)))
         if len(text) == limit: break
     return list(text)
 
 
 def to_vec(text, reshape=True):
     if isinstance(text, str): text = [text]
-    tokenized = numpy.array(__tokenizer.texts_to_sequences(list(text)))
+    tokenized = __numpy.array(__tokenizer.texts_to_sequences(list(text)))
     return tokenized.reshape(*tokenized.shape, 1) if reshape else tokenized
 
 
