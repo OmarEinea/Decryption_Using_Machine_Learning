1acb5fd6626c7fb130fccc15c3c550f15d1a9d33
Omar Einea <eineao@gmail.com>
2018-11-27 21:28:41 +0400

    Rewritten encryption methods using wrapper & lambda

--- encrypt.py
+++ encrypt.py
@@ -1,31 +1,34 @@
-from tools import __chars
-from math import ceil
+from tools import __chars, __named
 
 
-def caesar_cipher(string):
-    return ''.join([
-        __chars.list[(__chars.list.index(char) + 3) % __chars.length]
+@__named
+def caesar_cipher():
+    return lambda string: ''.join([
+        __chars[(__chars.index(char) + 3) % len(__chars)]
         for char in string
     ])
 
 
-def vigenere_cipher(string, key=None):
-    if key: key = key*ceil(len(string)/len(key))
-    else: key = (__chars.list*ceil(len(string)/len(__chars.list)))[1:]
-    return ''.join([
-        __chars.list[(__chars.list.index(char) + __chars.list.index(key[i])) % __chars.length]
+@__named
+def vigenere_cipher(key=None):
+    if not key: key = __chars
+    return lambda string: ''.join([
+        __chars[(__chars.index(char) + __chars.index(key[i % len(key)])) % len(__chars)]
         for i, char in enumerate(string)
     ])
 
 
-def shifting_cipher(string, by=1):
-    at = by % len(string)
-    return string[at:] + string[:at]
+@__named
+def shifting_cipher(by=1):
+    return lambda string: string[by:] + string[:by]
 
 
-def shifted_vigenere_cipher(string, by=5):
-    return shifting_cipher(vigenere_cipher(string), by)
+@__named
+def shifted_vigenere_cipher(by=5):
+    return lambda string: shifting_cipher(vigenere_cipher(string), by)
 
 
-def vigenere_shifted_cipher(string, by=5):
-    return vigenere_cipher(shifting_cipher(string, by))
+@__named
+def vigenere_shifted_cipher(by=5):
+    return lambda string: vigenere_cipher(shifting_cipher(string, by))
+
