d4e1ece6d90ac4bc0eea2918a7598d311a766fb5
Omar Einea <eineao@gmail.com>
2018-11-29 20:19:55 +0400

    Added trasposition cipher function

--- encrypt.py
+++ encrypt.py
@@ -31,3 +31,13 @@ def shifted_vigenere_cipher(by=5):
 @__named_lambda
 def vigenere_shifted_cipher(by=5):
     return lambda string: vigenere_cipher(shifting_cipher(string, by))
+
+
+@__named_lambda
+def transposition_cipher(order):
+    def transpose(sub):
+        if len(sub) < 8: sub = sub + '\t' * (8 - len(sub))
+        return ''.join(sub[-int(index)] for index in order).replace('\t', '')
+    check = set(order)
+    if len(check) != 8 or max(check) != '8' or min(check) != '1': raise Exception('Wrong Order!')
+    return lambda string: ''.join(transpose(string[i:i+8]) for i in range(0, len(string), 8))
